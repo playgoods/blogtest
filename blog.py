eb3cbcac30d012d22f6dd0621f7bb594c7b98367
playgoods <mohmed_e@hotmail.com>
2018-05-17 23:12:51 +0300

    the last chapter 2

--- blog.py
+++ blog.py
@@ -1,13 +1,28 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, url_for
 
 app = Flask(__name__)
 
-
+posts = [
+        { 
+            "author": "Mohamed Ali",
+            "title" : " the first blog",
+            "content" : " the test fake date for first blog ",
+            "date_posted" : "2018-05-16 19:10"
+        },
+        { 
+            "author": "Mohamed Ali",
+            "title" : " the second blog",
+            "content" : " the test fake date for second blog ",
+            "date_posted" : "2018-05-16 20:00"
+        }
+        ]
+@app.route('/home')
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html",posts=posts)
 
 @app.route('/about')
 def about():
 
-    return "<h1> about this page injoy </h1>"
+    return render_template("about.html", title=" the about blask ")
+
