2f71236b163b1c7b25e56006c282a65035250776
playgoods <mohmed_e@hotmail.com>
2018-05-19 06:25:59 +0300

    tutorail 3 but not complate

--- blog.py
+++ blog.py
@@ -1,6 +1,8 @@
 from flask import Flask , render_template, url_for
+from forms import RegistrationForm, Login
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '72a23248a0ebcf2bfda6d42f7f080a90'
 
 posts = [
         { 
@@ -26,3 +28,12 @@ def about():
 
     return render_template("about.html", title=" the about blask ")
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html",form=form, title = "Register" )
+
+@app.route('/login')
+def login():
+    form = Login()
+    return render_template("login.html",form=form, title = "Login" )
---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, PasswordField, SubmitField, BooleanField)
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField("User Name", validators = [DataRequired(), Length(2,20) ])
+    email = StringField("Email", validators = [DataRequired(), Email() ])
+    password = PasswordField("Password", validators = [DataRequired(), Length(5,20)])
+    confirm_password = PasswordField("Confrim Password", 
+        validators = [DataRequired(),EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+    
+
+class Login(FlaskForm):
+    email = StringField("Email", validators = [DataRequired(), Email() ])
+    password = PasswordField("Password", validators = [DataRequired(), Length(5,20)])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log In")
+
