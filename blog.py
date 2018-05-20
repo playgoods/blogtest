8214b1f77b1179e1383a7d01d0aa1cf70a0ebb08
playgoods <mohmed_e@hotmail.com>
2018-05-20 07:39:13 +0300

    still the tutorail 3 not compleate

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for
+from flask import Flask , render_template, url_for, flash, redirect
 from forms import RegistrationForm, Login
 
 app = Flask(__name__)
@@ -20,7 +20,7 @@ posts = [
         ]
 @app.route('/home')
 @app.route('/')
-def index():
+def home():
     return render_template("home.html",posts=posts)
 
 @app.route('/about')
@@ -28,12 +28,15 @@ def about():
 
     return render_template("about.html", title=" the about blask ")
 
-@app.route('/register')
+@app.route('/register', methods=["POST","GET"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account Created {form.username.data}","success")
+        return redirect(url_for('home'))
     return render_template("register.html",form=form, title = "Register" )
 
-@app.route('/login')
+@app.route('/login', methods=["POST","GET"])
 def login():
     form = Login()
     return render_template("login.html",form=form, title = "Login" )
