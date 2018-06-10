6324471b4fd4c2ef10e1efb0cbceffe9d779a3ff
mohamed ali <mohmed_e@hotmail.com>
2018-06-10 08:21:56 -0400

    the test

---
+++ blogtest/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '72a23248a0ebcf2bfda6d42f7f080a90'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+
+from blogtest import routs
