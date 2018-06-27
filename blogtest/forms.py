f19a281150e896f872093b1f9c71f462c73ae595
playgoods <mohmed_e@hotmail.com>
2018-06-27 15:16:47 +0300

    the tutorail 7 m 15

--- blogtest/forms.py
+++ blogtest/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import (StringField, PasswordField, SubmitField, BooleanField)
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from blogtest.models import User
@@ -26,3 +27,21 @@ class Login(FlaskForm):
     password = PasswordField("Password", validators = [DataRequired(), Length(5,20)])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("User Name", validators = [DataRequired(), Length(2,20) ])
+    email = StringField("Email", validators = [DataRequired(), Email() ])
+    submit = SubmitField("Update")
+
+    def validate_username(self,username):
+        if current_user.username != username.data :
+            user= User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken. Plase Choese a different one. ")
+
+    def validate_email(self,email):
+        if current_user.email != email.data :
+            user= User.query.filter_by(email=email.data).first()
+            if user:
+              raise ValidationError("That email is taken. Plase Choese a different one. ")
+
--- blogtest/routes.py
+++ blogtest/routes.py
@@ -1,5 +1,5 @@
 from flask import  render_template, url_for, flash, redirect, request
-from blogtest.forms import RegistrationForm, Login
+from blogtest.forms import RegistrationForm, Login, UpdateAccountForm
 from blogtest import app, bcrypt, db
 from blogtest.models import User,Post
 from flask_login import login_user, current_user,logout_user,login_required
@@ -66,4 +66,15 @@ def logout():
 @app.route('/account', methods=["POST","GET"])
 @login_required
 def account():
-    return render_template("account.html", title = "Account" )
+    form = UpdateAccountForm()
+    if form.valdiate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!","success")
+        return redirect(url_for("account"))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static',filename="profile_pics/"+ current_user.image_file)
+    return render_template("account.html", title = "Account", image_file=image_file,form =form )
